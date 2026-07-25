6ed5f4716f4954f941146427dc0e09808e7cc03a
uniquespace1 <a.zobur1158@miva.edu.ng>
2026-07-25 15:39:07 +0100

    Update planner.js

--- js/planner.js
+++ js/planner.js
@@ -1,1 +1,38 @@
+function addTask() {
 
+let input = document.getElementById("taskInput");
+
+let task = input.value.trim();
+
+if(task === ""){
+    alert("Please enter a task.");
+    return;
+}
+
+let li = document.createElement("li");
+
+li.innerHTML = `
+${task}
+<button onclick="completeTask(this)">Complete</button>
+<button onclick="deleteTask(this)">Delete</button>
+`;
+
+document.getElementById("taskList").appendChild(li);
+
+input.value = "";
+
+}
+
+function completeTask(button){
+
+button.parentElement.style.textDecoration = "line-through";
+
+button.parentElement.style.color = "green";
+
+}
+
+function deleteTask(button){
+
+button.parentElement.remove();
+
+}
